b21c56cd96c5494c5c9574344dc9919eaf871fc2
Tatsuma-Itou <0524.ito.tatsuma@gmail.com>
2016-11-11 00:07:07 +0900

    Update and rename echo_bot.php to callback.php

--- web/callback.php
+++ web/callback.php
@@ -18,8 +18,8 @@
 
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = '<your channel access token>';
-$channelSecret = '<your channel secret>';
+$channelAccessToken = 'KR5/LV6k4zm8mZpaw6U1fM8Isx6U+MzkgIH0EuMdYvlr8bAD2UK8uQ0aS5Q/Kn6OTgw8vxRXsYN4D9Hu0eT61tbDJdt/T7wGwY5VVLajSijR6F9X5yHT1GmDqc5HpNp57Bof/IDvzSDKj1WUmf5BCAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '04106210be1640325f6f8b23e03a4506';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
